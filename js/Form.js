8c96222b06a630ef709a315dac0e86282fb06fb8
kush463 <74005610+kush463@users.noreply.github.com>
2020-12-29 18:38:25 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,29 @@
+class Form {
+constructor(){
+
+}
+
+display(){
+var title = createElement('h2');
+title.html("car racing game");
+title.position(450,0);
+
+var input = createInput("name")
+input.position(450,160);
+var button = createButton("play")
+button.position(450,200);
+
+var greeting = createElement('h3');
+button.mousePressed(function(){
+    input.hide();
+    button.hide();
+    var name = input.value();
+    playerCount = playerCount + 1;
+    player.update(name);
+    player.updateCount(playerCount);
+    greeting.html("welcome " + name);
+    greeting.position(450,160);
+})
+}
+
+}
